a4c0361d313ea3aae9bf7d544bbb5044f6d9e2d9
Elan Ruusamäe <glen@pld-linux.org>
2011-08-13 04:16:45 +0000

    - cleanups, make compatible with perl 5.8
    Changed files:
        temper_mon.pl -> 1.2

--- temper_mon.pl
+++ temper_mon.pl
@@ -1,20 +1,10 @@
-#! /usr/bin/perl
-
-use 5.010;
+#!/usr/bin/perl
 use strict;
-use warnings;
-use Carp;
-use Device::USB;
-use Device::USB::PCSensor::HidTEMPer::Device;
-use Device::USB::PCSensor::HidTEMPer::NTC;
-use Device::USB::PCSensor::HidTEMPer::TEMPer; 
-use lib;
 use Device::USB::PCSensor::HidTEMPer;
 
 my $pcsensor  = Device::USB::PCSensor::HidTEMPer->new();
 my @devices   = $pcsensor->list_devices();
-  
-foreach my $device ( @devices )
-   {
-   say $device->internal()->celsius();
-   }
+
+foreach my $device (@devices) {
+	printf "Sensor Temperature C: %s\n", $device->internal()->celsius();
+}
